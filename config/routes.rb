73733f63314dcd46620406c1f9baf9b2f5579c24
mateusinacioo <mateus.inacio321@gmail.com>
2022-06-13 05:36:28 -0300

    created user private area

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
     resources :admins
   
   end
+
+  root to: "user/timeline#index"
+
+  namespace :user do
+    
+  
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
